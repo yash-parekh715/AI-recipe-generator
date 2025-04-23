e5e4f97dbe4c48b12ac99f236c689a445de9ec66
yash-parekh715 <parekhyash712005@gmail.com>
2025-04-23 16:50:21 +0530

    changes for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://ai-recipe-generator-wkwr.onrender.com/api";
 
 export const fetchItems = async () => {
   try {
